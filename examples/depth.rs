2bfa6a1370d623679fab99bba9464a547214a070
Yuhao Fang <fangyuhao@gmail.com>
2019-03-27 21:14:32 +0800

    adds depth examples

--- examples/depth.rs
+++ examples/depth.rs
@@ -1,1 +1,7 @@
-fn main() {}
+use huobi::{Function, Huobi};
+
+fn main() {
+    let huobi = Huobi::new("", "", 1957, "https://api.huobi.br.com");
+    let depth = huobi.depth("btcusdt", "step1").unwrap();
+    println!("{:?}", depth);
+}
